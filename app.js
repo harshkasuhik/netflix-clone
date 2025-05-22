24f138f6fc84de0936cb7bf7840309cba8d1902a
harshkasuhik <168298253+harshkasuhik@users.noreply.github.com>
2025-05-22 20:30:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+let firstname = prompt("enter your first name");
+let secondname = prompt("enter your last name ");
+let welcome = " we welcome you to this netflix clone made by harsh kaushik : "
+let h1 = welcome + firstname + secondname;
+console.log(alert(h1));
