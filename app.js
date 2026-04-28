831d5380e840d7efe883747a064abd37d0fb8586
akarshCL <akarshg222@gmail.com>
2026-04-28 14:29:39 +0530

    added spome mes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let firstname = prompt("enter your first name");
 let secondname = prompt("enter your last name ");
-let welcome = " we welcome you to this netflix clone made by harsh kaushik : "
+let welcome = " we welcome you to this netflix clone made by harsh kaushik and akarsh gupta : "
 let h1 = welcome + firstname + secondname;
+
 console.log(alert(h1));
