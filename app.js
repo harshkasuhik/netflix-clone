64044e399c25a606bf7a2641b917b415696ec06b
akarshCL <akarshg222@gmail.com>
2026-04-28 14:34:13 +0530

    ok

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let firstname = prompt("enter your first name");
-let secondname = prompt("enter your last name ");
+let firstname = prompt("Enter your first name");
+let secondname = prompt("Enter your last name ");
+
 let welcome = " we welcome you to this netflix clone made by harsh kaushik and akarsh gupta : "
 let h1 = welcome + firstname + secondname;
 
